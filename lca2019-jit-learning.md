8c8b8bdec165a85bb6a393648b95574a419d83dd
tessereth <tessereth@yahoo.com.au>
2019-01-08 20:05:27 +1100

    First pass at reveal.js template

---
+++ lca2019-jit-learning.md
@@ -0,0 +1,30 @@
+<!-- .slide: data-background-image="assets/lca-title-background.png" data-background-size="contain" class="title" -->
+
+### JIT Learning: It's great until it isn't
+<!-- .element style="font-weight: normal; padding-top: 5vh" -->
+
+<small style="margin-left: 30vw; margin-top: 6vh; font-size: x-large">Tessa Bradbury</small>
+
+
+
+<img data-src="assets/jit-learning-0.svg">
+
+
+
+<img data-src="assets/jit-learning-1.svg">
+
+
+
+<img data-src="assets/jit-learning-2.svg">
+
+
+
+<img data-src="assets/jit-learning-3.svg">
+
+
+
+<img data-src="assets/jit-learning-4.svg">
+
+
+
+<img data-src="assets/jit-learning-5.svg">
---
+++ slides.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+USAGE=$(cat <<-END
+Usage: $0 [markdown-file]
+END
+)
+
+if [[ $# -ne 1 || ! -f $1 ]]; then
+    echo $USAGE
+    exit 1
+fi
+
+SLIDES=$1
+
+if which yarn > /dev/null; then
+    JS_MANAGER=yarn
+elif which npm > /dev/null; then
+    JS_MANAGER=npm
+else
+    echo "Please install either npm or yarn and make it available on the path"
+    exit 1
+fi
+
+if [[ ! -e reveal.js ]]; then
+    git clone git@github.com:hakimel/reveal.js.git
+fi
+
+pushd reveal.js > /dev/null
+
+if [[ ! -e node_modules ]]; then
+    $JS_MANAGER install
+fi
+
+ln -sf ../index.html index.html
+ln -sf ../$SLIDES slides.md
+ln -sf ../assets assets
+
+$JS_MANAGER start
+
+popd > /dev/null
